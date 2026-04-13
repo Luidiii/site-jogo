25ac4db0ad44757ede619b1ac0bea6e42d7aa332
Luiz Felipe da Silva <luiz_silva@grupointegrado.br>
2026-04-12 23:22:13 -0300

    finalizado

--- script.js
+++ script.js
@@ -5,45 +5,7 @@ if (toggle && menu) {
     toggle.addEventListener('click', () => {
         menu.classList.toggle('active');
     });
-
-    const links = document.querySelectorAll('.menu a');
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            menu.classList.remove('active');
-        });
-    });
 }
-
-const botao = document.querySelector('.botao');
-if (botao) {
-    botao.addEventListener('click', () => {
-        botao.style.transform = "scale(0.95)";
-        setTimeout(() => {
-            botao.style.transform = "scale(1)";
-        }, 150);
-    });
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    if (form) {
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            const nome = document.getElementById("nome");
-            const email = document.getElementById("email");
-            const mensagem = document.getElementById("mensagem");
-            const status = document.getElementById("status");
-
-            if (!nome.value || !email.value || !mensagem.value) {
-                alert("❌ Preencha todos os campos!");
-                return;
-            }
-
-            status.innerHTML = "✅ Mensagem enviada com sucesso!";
-            form.reset();
-        });
-    }
-});
 
 const chavesProibidas = ["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
 
